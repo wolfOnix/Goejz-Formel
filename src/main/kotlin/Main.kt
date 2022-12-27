b87074eeda85f422164d92befd4e1703072f1369
robert <robertrobertzicku@yahoo.com>
2022-12-27 11:30:40 +0200

    Console messages updates: colouring and more natural language logs. Upgrades: Kotlin 1.7.21, Gradle 7.6.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,17 +2,31 @@
  * Copyright (c) 2022 Robert Kovats
  */
 
+import Colour.*
+
+private const val RST = "\u001b[0m"
+
+private infix fun String.coloured(colour: Colour): String = "${colour.code}$this$RST"
+
+private enum class Colour(val code: String) {
+    CYAN("\u001b[36m"),
+    RED("\u001b[31m"),
+    YELLOW("\u001b[33m")
+}
+
 fun main() {
     println("Guten Tag! Rechnen Sie das Göjz Funktion, indem Sie fügen die notwendige Daten unten hinzu.\n")
     try {
-        print("i = "); val i = readInt(defaultValue = 1)
-        print("s(i) = "); val si = readInt()
-        print("iMax = "); val max = readInt(defaultValue = 10)
+        print("Sie wissen, dass die Person mit der Ordnungsnummer ... "); val i = readInt(defaultValue = 1)
+        print("hat die Summe von ... bezahlen "); val si = readInt()
+        print("und Sie wünschen die Tabelle bis zu der Person mit der Ordnungsnummer ... herauszufinden "); val max = readInt(defaultValue = 10)
 
         println("\n\tDie Göjz Tabelle sieht so aus:\n")
 
         goejzDescending(i = i, si = si.toFloat(), doPrint = false)
         goejzAscending(i = i, si = si.toFloat(), iMax = max)
+
+        println("\nNa ja, das war's!" coloured CYAN)
     } catch (e: Exception) {
         println(e.message)
     }
@@ -34,10 +48,11 @@ private fun printGoejz(i: Int, si: Float) {
 }
 
 private fun readInt(defaultValue: Int? = null): Int {
-    val line = readLine() ?: throw EOFReachedExc()
+    val line = readlnOrNull() ?: throw EOFReachedExc()
     return try {
         line.toInt()
     } catch (e: Exception) {
-        defaultValue ?: throw InvalidFormatExc("'$line' ist keine gültige Nummer.")
+        defaultValue?.also { println("\tOK, das wird $defaultValue" coloured YELLOW) }
+            ?: throw InvalidFormatExc("\t'$line' ist keine gültige Nummer." coloured RED)
     }
 }
